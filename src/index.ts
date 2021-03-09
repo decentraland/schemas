3c950b7241efd06ae1642aa7fa13dd53b33fa6db
Ignacio Mazzara <nachomazzara@gmail.com>
2021-03-09 18:29:44 -0300

    feat: export

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export * from './validation'
 
 // export all the types
 export { ChainId } from './dapps/chain-id'
+export { ChainNames } from './dapps/chain-names'
 export { Network } from './dapps/network'
 export { MetaTransaction } from './dapps/meta-transactions'
