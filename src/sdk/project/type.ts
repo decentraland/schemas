3e9203212a7b76d04dbcc1ff755a2866c3644fbd
gonpombo8 <gonzalo@decentraland.org>
2021-11-24 12:17:15 -0300

    fix sdk namespace

---
+++ src/sdk/project/type.ts
@@ -0,0 +1,18 @@
+import { generateValidator, JSONSchema, ValidateFunction } from "../../validation";
+
+/* @internal */
+export enum ProjectType {
+  SCENE = 'scene',
+  SMART_ITEM = 'smart-item',
+  PORTABLE_EXPERIENCE = 'portable-experience'
+}
+
+/* @internal */
+export namespace ProjectType {
+  export const schema: JSONSchema<ProjectType> = {
+    type: "string",
+    enum: [ProjectType]
+  };
+
+  export const validate: ValidateFunction<ProjectType> = generateValidator(schema);
+}
