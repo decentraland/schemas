9ab7a5ac6dff295b49783d1562d0b09c8aab05f1
Juan Cazala <juan@decentraland.org>
2021-11-25 15:48:42 -0300

    feat: add Store schema (#28)
    * feat: add Store schema
    
    * chore: build before checking api

---
+++ src/dapps/store.ts
@@ -0,0 +1,69 @@
+import { generateValidator, JSONSchema, ValidateFunction } from '../validation'
+
+export type Store = {
+  id: string // urn
+  owner: string
+  description: string
+  links: {
+    name: string
+    url: string
+  }[]
+  images: {
+    name: string
+    file: string
+  }[]
+  version: number
+}
+
+export namespace Store {
+  export const schema: JSONSchema<Store> = {
+    type: 'object',
+    properties: {
+      id: {
+        type: 'string',
+      },
+      version: {
+        type: 'number',
+      },
+      owner: {
+        type: 'string',
+      },
+      description: {
+        type: 'string',
+      },
+      links: {
+        type: 'array',
+        items: {
+          type: 'object',
+          properties: {
+            name: {
+              type: 'string',
+            },
+            url: {
+              type: 'string',
+            },
+          },
+          required: ['name', 'url'],
+        },
+      },
+      images: {
+        type: 'array',
+        items: {
+          type: 'object',
+          properties: {
+            name: {
+              type: 'string',
+            },
+            file: {
+              type: 'string',
+            },
+          },
+          required: ['name', 'file'],
+        },
+      },
+    },
+    required: ['id', 'version', 'owner', 'description', 'links', 'images'],
+  }
+
+  export const validate: ValidateFunction<Store> = generateValidator(schema)
+}
