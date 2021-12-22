a2d4b0aa9505a68c7b2d90c6b0c508c8967c7630
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-12-22 16:58:51 -0300

    feat: Reformat

--- test/account.spec.ts
+++ test/account.spec.ts
@@ -1,23 +1,23 @@
-import expect from "expect";
-import { Account } from "../src";
-import { testTypeSignature } from "./test-utils";
+import expect from 'expect'
+import { Account } from '../src'
+import { testTypeSignature } from './test-utils'
 
-describe("Account tests", () => {
+describe('Account tests', () => {
   const account: Account = {
-    id: "0x2041694201525630780780247644590609268611",
-    address: "0x2041694201525630780780247644590609268611",
+    id: '0x2041694201525630780780247644590609268611',
+    address: '0x2041694201525630780780247644590609268611',
     sales: 10,
     purchases: 10,
-    spent: "1000000000000000000",
-    earned: "1000000000000000000",
-    royalties: "1000000000000000000",
-  };
+    spent: '1000000000000000000',
+    earned: '1000000000000000000',
+    royalties: '1000000000000000000'
+  }
 
-  testTypeSignature(Account, account);
+  testTypeSignature(Account, account)
 
-  it("static tests must pass", () => {
-    expect(Account.validate(account)).toEqual(true);
-    expect(Account.validate(null)).toEqual(false);
-    expect(Account.validate({})).toEqual(false);
-  });
-});
+  it('static tests must pass', () => {
+    expect(Account.validate(account)).toEqual(true)
+    expect(Account.validate(null)).toEqual(false)
+    expect(Account.validate({})).toEqual(false)
+  })
+})
