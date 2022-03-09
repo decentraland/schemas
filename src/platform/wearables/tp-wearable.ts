d85efb88e7675e17db5eaa1aed464811617e103f
Lautaro Petaccio <lausuper@gmail.com>
2022-03-09 12:27:26 -0300

    Re-use validations

--- src/platform/wearables/tp-wearable.ts
+++ src/platform/wearables/tp-wearable.ts
@@ -1,8 +1,4 @@
-import {
-  generateValidator,
-  JSONSchema,
-  ValidateFunction
-} from '../../validation'
+import { JSONSchema, ValidateFunction } from '../../validation'
 import { BaseWearable } from './base-wearable'
 
 /** @alpha */
@@ -10,16 +6,6 @@ export type TPWearable = BaseWearable
 
 /** @alpha */
 export namespace TPWearable {
-  export const schema: JSONSchema<TPWearable> = {
-    ...BaseWearable.schema
-  }
-
-  const schemaValidator: ValidateFunction<TPWearable> =
-    generateValidator(schema)
-  export const validate: ValidateFunction<TPWearable> = (
-    wearable: any
-  ): wearable is TPWearable =>
-    schemaValidator(wearable) &&
-    BaseWearable.validateDuplicatedLocales(wearable.descriptions) &&
-    BaseWearable.validateDuplicatedLocales(wearable.names)
+  export const schema: JSONSchema<TPWearable> = BaseWearable.schema
+  export const validate: ValidateFunction<TPWearable> = BaseWearable.validate
 }
