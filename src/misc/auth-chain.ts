fd9172a1ef659a80ec3291f747e473ccf68d6077
menduz <github@menduz.com>
2022-05-20 18:08:23 -0300

    feat: add DeploymentWithAuthChain and AuthChain

---
+++ src/misc/auth-chain.ts
@@ -0,0 +1,66 @@
+import { JSONSchema, generateValidator, ValidateFunction } from "../validation"
+
+/**
+ * @public
+ */
+export type AuthLink = {
+  type: AuthLinkType
+  payload: string
+  signature: string
+}
+
+/**
+ * @public
+ */
+export enum AuthLinkType {
+  "SIGNER" = "SIGNER",
+  "ECDSA_PERSONAL_EPHEMERAL" = "ECDSA_PERSONAL_EPHEMERAL",
+  "ECDSA_PERSONAL_SIGNED_ENTITY" = "ECDSA_PERSONAL_SIGNED_ENTITY",
+  /**
+   * See https://github.com/ethereum/EIPs/issues/1654
+   */
+  "ECDSA_EIP_1654_EPHEMERAL" = "ECDSA_EIP_1654_EPHEMERAL",
+  /**
+   * See https://github.com/ethereum/EIPs/issues/1654
+   */
+  "ECDSA_EIP_1654_SIGNED_ENTITY" = "ECDSA_EIP_1654_SIGNED_ENTITY",
+}
+
+/**
+ * @public
+ */
+export namespace AuthLink {
+  export const schema: JSONSchema<AuthLink> = {
+    type: "object",
+    properties: {
+      type: {
+        type: "string",
+        enum: Object.values(AuthLinkType),
+      },
+      payload: { type: "string" },
+      signature: { type: "string" },
+    },
+    required: ["payload", "signature", "type"],
+  }
+
+  export const validate: ValidateFunction<AuthLink> = generateValidator(schema)
+}
+
+/**
+ * AuthChain is an array of elements used to create and verify signatures
+ * and ephemeral keys.
+ *
+ * @public
+ */
+export type AuthChain = AuthLink[]
+
+/** @public */
+export namespace AuthChain {
+  export const schema: JSONSchema<AuthChain> = {
+    type: "array",
+    items: AuthLink.schema,
+    minItems: 1
+  }
+
+  export const validate: ValidateFunction<AuthChain> = generateValidator(schema)
+}
---
+++ src/platform/deployment-with-auth-chain.ts
@@ -0,0 +1,34 @@
+import { AuthChain } from "../misc/auth-chain"
+import { generateValidator, JSONSchema, ValidateFunction } from "../validation"
+
+/**
+ * This type describes the minimum deployment + AuthChain needed to synchronize
+ * a deployed entity across catalysts.
+ * @public
+ */
+export type DeploymentWithAuthChain = {
+  entityId: string
+  entityType: string
+  pointers: string[]
+  localTimestamp: number
+  authChain: AuthChain
+}
+
+/**
+ * @public
+ */
+export namespace DeploymentWithAuthChain {
+  export const schema: JSONSchema<DeploymentWithAuthChain> = {
+    type: "object",
+    properties: {
+      entityId: { type: "string" },
+      entityType: { type: "string" },
+      pointers: { type: "array", items: { type: "string" }, minItems: 1 },
+      localTimestamp: { type: "number", minimum: 0 },
+      authChain: AuthChain.schema,
+    },
+    required: ["entityId", "entityType", "pointers", "localTimestamp", "authChain"],
+  }
+
+  export const validate: ValidateFunction<DeploymentWithAuthChain> = generateValidator(schema)
+}
