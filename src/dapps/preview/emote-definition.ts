de81ae04d9849405d5669b7eaac0307b6bcf224d
Juanma Hidalgo <juanma06@gmail.com>
2022-07-27 16:34:01 +0200

    feat: dApps - Add EmoteDefinition and representation definition (#111)
    * feat: dApps - Add EmoteDefinition and representation definition
    
    * feat: fix eslint issue
    
    * feat: update the schemas.api

---
+++ src/dapps/preview/emote-definition.ts
@@ -0,0 +1,18 @@
+import { Emote, EmoteRepresentationADR74 } from '../../platform'
+
+/** @alpha */
+export type EmoteRepresentationDefinition = Omit<
+  EmoteRepresentationADR74,
+  'contents'
+> & {
+  contents: {
+    key: string
+    url: string
+  }[]
+}
+
+export type EmoteDefinition = Omit<Emote, 'emoteDataADR74'> & {
+  emoteDataADR74: Omit<Emote['emoteDataADR74'], 'representations'> & {
+    representations: EmoteRepresentationDefinition[]
+  }
+}
