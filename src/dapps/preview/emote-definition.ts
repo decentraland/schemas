ef94da9a82280165da520aea5fe622d98554d684
Juanma Hidalgo <juanma06@gmail.com>
2022-07-27 16:22:06 +0200

    feat: fix eslint issue

--- src/dapps/preview/emote-definition.ts
+++ src/dapps/preview/emote-definition.ts
@@ -1,18 +1,18 @@
-import { Emote, EmoteRepresentationADR74 } from "../../platform";
+import { Emote, EmoteRepresentationADR74 } from '../../platform'
 
 /** @alpha */
 export type EmoteRepresentationDefinition = Omit<
   EmoteRepresentationADR74,
-  "contents"
+  'contents'
 > & {
   contents: {
-    key: string;
-    url: string;
-  }[];
-};
+    key: string
+    url: string
+  }[]
+}
 
-export type EmoteDefinition = Omit<Emote, "emoteDataADR74"> & {
-  emoteDataADR74: Omit<Emote["emoteDataADR74"], "representations"> & {
-    representations: EmoteRepresentationDefinition[];
-  };
-};
+export type EmoteDefinition = Omit<Emote, 'emoteDataADR74'> & {
+  emoteDataADR74: Omit<Emote['emoteDataADR74'], 'representations'> & {
+    representations: EmoteRepresentationDefinition[]
+  }
+}
