c351ac841028797d69dba1331dc289567e6a4d7d
Leandro Mendoza <leandro@decentraland.org>
2022-08-26 17:07:18 -0300

    feat: remove asset-json (the wrong wearable.json) and extend the current used wearable.json

--- src/sdk/project/index.ts
+++ src/sdk/project/index.ts
@@ -1,2 +1,2 @@
 export * from './type'
-export * from './asset-json'
+export * from './wearable-json'
---
+++ src/sdk/project/wearable-json.ts
@@ -0,0 +1,69 @@
+import { Rarity } from '../../dapps/rarity'
+import { WearableCategory } from '../../dapps/wearable-category'
+import { WearableRepresentation } from '../../platform/item/wearable/representation'
+import { Wearable } from '../../platform/item/wearable/wearable'
+import {
+  generateLazyValidator,
+  JSONSchema,
+  ValidateFunction
+} from '../../validation'
+
+/**
+ * @alpha
+ */
+export type WearableJson = Pick<Wearable, 'data' | 'name' | 'description'> & {
+  rarity: Rarity
+}
+
+/**
+ * @alpha
+ */
+export namespace WearableJson {
+  export const schema: JSONSchema<WearableJson> = {
+    type: 'object',
+    properties: {
+      description: {
+        type: 'string'
+      },
+      rarity: {
+        ...Rarity.schema,
+        nullable: true
+      },
+      name: {
+        type: 'string'
+      },
+      data: {
+        type: 'object',
+        properties: {
+          replaces: {
+            type: 'array',
+            items: WearableCategory.schema
+          },
+          hides: {
+            type: 'array',
+            items: WearableCategory.schema
+          },
+          tags: {
+            type: 'array',
+            items: {
+              type: 'string',
+              minLength: 1
+            }
+          },
+          representations: {
+            type: 'array',
+            items: WearableRepresentation.schema,
+            minItems: 1
+          },
+          category: WearableCategory.schema
+        },
+        required: ['replaces', 'hides', 'tags', 'representations', 'category']
+      }
+    },
+    additionalProperties: true,
+    required: ['description', 'name', 'data']
+  }
+
+  export const validate: ValidateFunction<WearableJson> =
+    generateLazyValidator(schema)
+}
