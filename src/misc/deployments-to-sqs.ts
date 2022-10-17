0633e20c4d237f3c41f909c4909c45f5fe1783b4
Mariano Goldman <marianogoldman@users.noreply.github.com>
2022-10-17 17:56:11 -0300

    feat: deployments to sqs (#137)

---
+++ src/misc/deployments-to-sqs.ts
@@ -0,0 +1,47 @@
+import {
+  JSONSchema,
+  generateLazyValidator,
+  ValidateFunction
+} from '../validation'
+import { AuthChain } from './auth-chain'
+
+/**
+ * @public
+ */
+export type DeploymentToSqs = {
+  entity: {
+    entityId: string
+    authChain: AuthChain
+  }
+  contentServerUrls?: string[]
+}
+
+/**
+ * @public
+ */
+export namespace DeploymentToSqs {
+  export const schema: JSONSchema<DeploymentToSqs> = {
+    type: 'object',
+    required: ['entity'],
+    properties: {
+      entity: {
+        type: 'object',
+        required: ['entityId', 'authChain'],
+        properties: {
+          entityId: { type: 'string' },
+          authChain: AuthChain.schema
+        },
+        additionalProperties: true
+      },
+      contentServerUrls: {
+        type: 'array',
+        items: { type: 'string' },
+        nullable: true
+      }
+    },
+    additionalProperties: true
+  }
+
+  export const validate: ValidateFunction<DeploymentToSqs> =
+    generateLazyValidator(schema)
+}
