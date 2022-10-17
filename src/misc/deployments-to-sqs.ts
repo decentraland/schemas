94406c9a9e6dae12a10060000a7eb6af1697c44d
Mariano Goldman <mariano.goldman@decentraland.org>
2022-10-17 17:24:44 -0300

    chore: deployments to sqs

---
+++ src/misc/deployments-to-sqs.ts
@@ -0,0 +1,58 @@
+import {
+  JSONSchema,
+  generateLazyValidator,
+  ValidateFunction
+} from '../validation'
+import { AuthChain } from "./auth-chain";
+
+/**
+ * @public
+ */
+export type DeploymentToSqs = {
+  entity: Entity
+  baseUrls: string[]
+}
+
+/**
+ * @public
+ */
+export type Entity = {
+  entityId: string
+  authChain: AuthChain
+}
+
+/**
+ * @public
+ */
+export namespace Entity {
+  export const schema: JSONSchema<Entity> = {
+    type: 'object',
+    required: [
+      'entityId',
+      'authChain',
+    ],
+    properties: {
+      entityId: { type: 'string' },
+      authChain: AuthChain.schema
+    },
+    additionalProperties: true
+  }
+}
+
+/**
+ * @public
+ */
+export namespace DeploymentToSqs {
+  export const schema: JSONSchema<DeploymentToSqs> = {
+    type: 'object',
+    required: ['entity'],
+    properties: {
+      entity: Entity.schema,
+      baseUrls:  { type: 'array', items: { type: 'string' } }
+    },
+    additionalProperties: true
+  }
+
+  export const validate: ValidateFunction<DeploymentToSqs> =
+    generateLazyValidator(schema)
+}
