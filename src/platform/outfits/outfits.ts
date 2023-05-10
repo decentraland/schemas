fd0347cb9858170f11db5f932613608b6199c96c
Pedro Tamborindeguy <pftambo@gmail.com>
2023-05-10 16:56:56 -0300

    feat: add outfits

---
+++ src/platform/outfits/outfits.ts
@@ -0,0 +1,102 @@
+import { Color3, WearableId } from '../../misc'
+import {
+  JSONSchema,
+  ValidateFunction,
+  generateLazyValidator
+} from '../../validation'
+
+/**
+ * @alpha
+ */
+export type Outfit = {
+  bodyShape: WearableId
+  eyes: { color: Color3 }
+  hair: { color: Color3 }
+  skin: { color: Color3 }
+  wearables: WearableId[]
+}
+
+/**
+ * Outfit
+ * @alpha
+ */
+export namespace Outfit {
+  export const schema: JSONSchema<Outfit> = {
+    type: 'object',
+    required: ['bodyShape', 'eyes', 'hair', 'skin', 'wearables'],
+    properties: {
+      bodyShape: {
+        type: 'string'
+      },
+      eyes: {
+        type: 'object',
+        required: ['color'],
+        properties: {
+          color: Color3.schema
+        }
+      },
+      hair: {
+        type: 'object',
+        required: ['color'],
+        properties: {
+          color: Color3.schema
+        }
+      },
+      skin: {
+        type: 'object',
+        required: ['color'],
+        properties: {
+          color: Color3.schema
+        }
+      },
+      wearables: {
+        type: 'array',
+        items: {
+          type: 'string'
+        }
+      }
+    },
+    additionalProperties: true
+  }
+  export const validate: ValidateFunction<Outfit> =
+    generateLazyValidator(schema)
+}
+
+/**
+ * @alpha
+ */
+export type Outfits = {
+  outfits: {
+    slot: number
+    outfit: Outfit
+  }[]
+}
+
+/**
+ * Avatar
+ * @alpha
+ */
+export namespace Outfits {
+  export const schema: JSONSchema<Outfits> = {
+    type: 'object',
+    required: ['outfits'],
+    properties: {
+      outfits: {
+        type: 'array',
+        items: {
+          type: 'object',
+          required: ['slot', 'outfit'],
+          properties: {
+            slot: {
+              type: 'number'
+            },
+            outfit: Outfit.schema
+          }
+        }
+      }
+    },
+    additionalProperties: true
+  }
+  export const validate: ValidateFunction<Outfits> =
+    generateLazyValidator(schema)
+}
---
+++ test/platform/item/outfits/outfits.spec.ts
@@ -0,0 +1,127 @@
+import expect from 'expect'
+import { Outfits } from '../../../../src'
+import { testTypeSignature } from '../../../test-utils'
+
+const OUTFITS: Outfits = {
+  outfits: [
+    {
+      slot: 1,
+      outfit: {
+        bodyShape: 'urn:decentraland:off-chain:base-avatars:BaseMale',
+        eyes: { color: { r: 0.23046875, g: 0.625, b: 0.3125 } },
+        hair: { color: { r: 0.35546875, g: 0.19140625, b: 0.05859375 } },
+        skin: { color: { r: 0.94921875, g: 0.76171875, b: 0.6484375 } },
+        wearables: [
+          'urn:decentraland:off-chain:base-avatars:tall_front_01',
+          'urn:decentraland:off-chain:base-avatars:eyes_08',
+          'urn:decentraland:off-chain:base-avatars:eyebrows_00',
+          'urn:decentraland:off-chain:base-avatars:mouth_05',
+          'urn:decentraland:matic:collections-v2:0xf6f601efee04e74cecac02c8c5bdc8cc0fc1c721:0',
+          'urn:decentraland:off-chain:base-avatars:classic_shoes',
+          'urn:decentraland:off-chain:base-avatars:red_tshirt',
+          'urn:decentraland:off-chain:base-avatars:trash_jean'
+        ]
+      }
+    }
+  ]
+}
+
+it('outfits is valid', () => {
+  testTypeSignature(Outfits, OUTFITS)
+  expect(Outfits.validate(OUTFITS)).toEqual(true)
+})
+
+it('null outfits is invalid', () => {
+  expect(Outfits.validate(null)).toEqual(false)
+})
+
+it('empty object outfits is invalid', () => {
+  expect(Outfits.validate({})).toEqual(false)
+})
+
+it('empty object outfits is invalid', () => {
+  expect(Outfits.validate({})).toEqual(false)
+})
+
+it('outfits with not outfits is valid', () => {
+  expect(
+    Outfits.validate({
+      outfits: []
+    })
+  ).toEqual(true)
+})
+
+it('outfit without bodyShape is invalid', () => {
+  const outfit = OUTFITS.outfits[0].outfit
+  const outfitWithoutBodyShape = {
+    eyes: outfit.eyes,
+    hair: outfit.hair,
+    skin: outfit.skin,
+    wearables: outfit.wearables
+  }
+  expect(
+    Outfits.validate({
+      outfits: [{ slot: 1, outfit: outfitWithoutBodyShape }]
+    })
+  ).toEqual(false)
+})
+
+it('outfit without eyes is invalid', () => {
+  const outfit = OUTFITS.outfits[0].outfit
+  const outfitWithoutEyes = {
+    bodyShape: outfit.bodyShape,
+    hair: outfit.hair,
+    skin: outfit.skin,
+    wearables: outfit.wearables
+  }
+  expect(
+    Outfits.validate({
+      outfits: [{ slot: 1, outfit: outfitWithoutEyes }]
+    })
+  ).toEqual(false)
+})
+
+it('outfit without hair is invalid', () => {
+  const outfit = OUTFITS.outfits[0].outfit
+  const outfitWithoutHair = {
+    bodyShape: outfit.bodyShape,
+    eyes: outfit.eyes,
+    skin: outfit.skin,
+    wearables: outfit.wearables
+  }
+  expect(
+    Outfits.validate({
+      outfits: [{ slot: 1, outfit: outfitWithoutHair }]
+    })
+  ).toEqual(false)
+})
+
+it('outfit without skin is invalid', () => {
+  const outfit = OUTFITS.outfits[0].outfit
+  const outfitWithoutSkin = {
+    bodyShape: outfit.bodyShape,
+    eyes: outfit.eyes,
+    hair: outfit.hair,
+    wearables: outfit.wearables
+  }
+  expect(
+    Outfits.validate({
+      outfits: [{ slot: 1, outfit: outfitWithoutSkin }]
+    })
+  ).toEqual(false)
+})
+
+it('outfit without wearables is invalid', () => {
+  const outfit = OUTFITS.outfits[0].outfit
+  const outfitWithoutWearables = {
+    bodyShape: outfit.bodyShape,
+    eyes: outfit.eyes,
+    hair: outfit.hair,
+    skin: outfit.skin
+  }
+  expect(
+    Outfits.validate({
+      outfits: [{ slot: 1, outfit: outfitWithoutWearables }]
+    })
+  ).toEqual(false)
+})
