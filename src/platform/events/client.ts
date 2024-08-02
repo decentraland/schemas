7ac893fb5e04eabf06598da433ce20899c85b17b
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2024-08-02 17:20:11 -0300

    fix: MoveToParcel event definition (#291)

--- src/platform/events/client.ts
+++ src/platform/events/client.ts
@@ -1,12 +1,12 @@
 import { EthAddress } from '../../misc'
-import { AuthLink } from '../../misc/auth-chain'
+import { AuthChain } from '../../misc/auth-chain'
 import { BaseEvent, Events } from './base'
 
 export type MoveToParcelEvent = BaseEvent & {
   type: Events.Type.CLIENT
   subType: Events.SubType.Client.MOVE_TO_PARCEL
   metadata: {
-    authChain: AuthLink
+    authChain: AuthChain
     parcel: {
       isEmptyParcel: boolean
       newParcel: string
