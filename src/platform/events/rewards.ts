5491c09f9c1f6a165f50158c803a27bc57397242
Kevin Szuchet <kevin.szuchet@decentraland.org>
2024-08-27 13:13:33 +0200

    refactor: Create a fn createSchema and reuse it in the different namespaces

--- src/platform/events/rewards.ts
+++ src/platform/events/rewards.ts
@@ -21,6 +21,8 @@ export type CampaignWellKnownIssueEventMetadata = BaseEventMetadata & {
   campaignName: string
 }
 
+type Metadata = RewardEventMetadata | CampaignWellKnownIssueEventMetadata
+
 export type RewardInProgressEvent = BaseEvent & {
   type: Events.Type.REWARDS
   subType: Events.SubType.Rewards.REWARD_IN_PROGRESS
@@ -57,170 +59,93 @@ export type RewardDelayedEvent = BaseEvent & {
   metadata: RewardEventMetadata
 }
 
-export namespace RewardInProgressEvent {
-  export const schema: JSONSchema<RewardInProgressEvent> = {
+function createSchema<T>(subType: string, metadataSchema: JSONSchema<Metadata>): JSONSchema<T> {
+  return {
     type: 'object',
     properties: {
       type: { type: 'string', const: Events.Type.REWARDS },
-      subType: { type: 'string', const: Events.SubType.Rewards.REWARD_IN_PROGRESS },
+      subType: { type: 'string', const: subType },
       key: { type: 'string' },
       timestamp: { type: 'number', minimum: 0 },
-      metadata: {
-        type: 'object',
-        properties: {
-          title: { type: 'string' },
-          description: { type: 'string' },
-          beneficiary: { type: 'string' },
-          tokenName: { type: 'string' },
-          tokenImage: { type: 'string' },
-          tokenRarity: { type: 'string', nullable: true },
-          tokenCategory: { type: 'string', nullable: true }
-        },
-        required: ['title', 'description', 'beneficiary', 'tokenName', 'tokenImage']
-      }
+      metadata: metadataSchema
     },
     required: ['type', 'subType', 'key', 'timestamp', 'metadata'],
     additionalProperties: false
-  }
+  } as JSONSchema<T>
+}
 
+const rewardEventMetadataSchema: JSONSchema<RewardEventMetadata> = {
+  type: 'object',
+  properties: {
+    title: { type: 'string' },
+    description: { type: 'string' },
+    beneficiary: { type: 'string' },
+    tokenName: { type: 'string' },
+    tokenImage: { type: 'string' },
+    tokenRarity: { type: 'string', nullable: true },
+    tokenCategory: { type: 'string', nullable: true }
+  },
+  required: ['title', 'description', 'beneficiary', 'tokenName', 'tokenImage'],
+  additionalProperties: false
+}
+
+const campaignEventMetadataSchema: JSONSchema<CampaignWellKnownIssueEventMetadata> = {
+  type: 'object',
+  properties: {
+    title: { type: 'string' },
+    description: { type: 'string' },
+    owner: { type: 'string' },
+    campaignId: { type: 'string' },
+    campaignName: { type: 'string' }
+  },
+  required: ['title', 'description', 'owner', 'campaignId', 'campaignName'],
+  additionalProperties: false
+}
+
+export namespace RewardInProgressEvent {
+  export const schema: JSONSchema<RewardInProgressEvent> = createSchema(
+    Events.SubType.Rewards.REWARD_IN_PROGRESS,
+    rewardEventMetadataSchema
+  )
   export const validate: ValidateFunction<RewardInProgressEvent> = generateLazyValidator(schema)
 }
 
 export namespace RewardAssignedEvent {
-  export const schema: JSONSchema<RewardAssignedEvent> = {
-    type: 'object',
-    properties: {
-      type: { type: 'string', const: Events.Type.REWARDS },
-      subType: { type: 'string', const: Events.SubType.Rewards.REWARD_ASSIGNED },
-      key: { type: 'string' },
-      timestamp: { type: 'number', minimum: 0 },
-      metadata: {
-        type: 'object',
-        properties: {
-          title: { type: 'string' },
-          description: { type: 'string' },
-          beneficiary: { type: 'string' },
-          tokenName: { type: 'string' },
-          tokenImage: { type: 'string' },
-          tokenRarity: { type: 'string', nullable: true },
-          tokenCategory: { type: 'string', nullable: true }
-        },
-        required: ['title', 'description', 'beneficiary', 'tokenName', 'tokenImage']
-      }
-    },
-    required: ['type', 'subType', 'key', 'timestamp', 'metadata'],
-    additionalProperties: false
-  }
-
+  export const schema: JSONSchema<RewardAssignedEvent> = createSchema(
+    Events.SubType.Rewards.REWARD_ASSIGNED,
+    rewardEventMetadataSchema
+  )
   export const validate: ValidateFunction<RewardAssignedEvent> = generateLazyValidator(schema)
 }
 
 export namespace CampaignOutOfFundsEvent {
-  export const schema: JSONSchema<CampaignOutOfFundsEvent> = {
-    type: 'object',
-    properties: {
-      type: { type: 'string', const: Events.Type.REWARDS },
-      subType: { type: 'string', const: Events.SubType.Rewards.CAMPAIGN_OUT_OF_FUNDS },
-      key: { type: 'string' },
-      timestamp: { type: 'number', minimum: 0 },
-      metadata: {
-        type: 'object',
-        properties: {
-          title: { type: 'string' },
-          description: { type: 'string' },
-          owner: { type: 'string' },
-          campaignId: { type: 'string' },
-          campaignName: { type: 'string' }
-        },
-        required: ['title', 'description', 'owner', 'campaignId', 'campaignName']
-      }
-    },
-    required: ['type', 'subType', 'key', 'timestamp', 'metadata'],
-    additionalProperties: false
-  }
-
+  export const schema: JSONSchema<CampaignOutOfFundsEvent> = createSchema(
+    Events.SubType.Rewards.CAMPAIGN_OUT_OF_FUNDS,
+    campaignEventMetadataSchema
+  )
   export const validate: ValidateFunction<CampaignOutOfFundsEvent> = generateLazyValidator(schema)
 }
 
 export namespace CampaignGasPriceHigherThanExpectedEvent {
-  export const schema: JSONSchema<CampaignGasPriceHigherThanExpectedEvent> = {
-    type: 'object',
-    properties: {
-      type: { type: 'string', const: Events.Type.REWARDS },
-      subType: { type: 'string', const: Events.SubType.Rewards.CAMPAIGN_GAS_PRICE_HIGHER_THAN_EXPECTED },
-      key: { type: 'string' },
-      timestamp: { type: 'number', minimum: 0 },
-      metadata: {
-        type: 'object',
-        properties: {
-          title: { type: 'string' },
-          description: { type: 'string' },
-          owner: { type: 'string' },
-          campaignId: { type: 'string' },
-          campaignName: { type: 'string' }
-        },
-        required: ['title', 'description', 'owner', 'campaignId', 'campaignName']
-      }
-    },
-    required: ['type', 'subType', 'key', 'timestamp', 'metadata'],
-    additionalProperties: false
-  }
-
+  export const schema: JSONSchema<CampaignGasPriceHigherThanExpectedEvent> = createSchema(
+    Events.SubType.Rewards.CAMPAIGN_GAS_PRICE_HIGHER_THAN_EXPECTED,
+    campaignEventMetadataSchema
+  )
   export const validate: ValidateFunction<CampaignGasPriceHigherThanExpectedEvent> = generateLazyValidator(schema)
 }
 
 export namespace CampaignOutOfStockEvent {
-  export const schema: JSONSchema<CampaignOutOfStockEvent> = {
-    type: 'object',
-    properties: {
-      type: { type: 'string', const: Events.Type.REWARDS },
-      subType: { type: 'string', const: Events.SubType.Rewards.CAMPAIGN_OUT_OF_STOCK },
-      key: { type: 'string' },
-      timestamp: { type: 'number', minimum: 0 },
-      metadata: {
-        type: 'object',
-        properties: {
-          title: { type: 'string' },
-          description: { type: 'string' },
-          owner: { type: 'string' },
-          campaignId: { type: 'string' },
-          campaignName: { type: 'string' }
-        },
-        required: ['title', 'description', 'owner', 'campaignId', 'campaignName']
-      }
-    },
-    required: ['type', 'subType', 'key', 'timestamp', 'metadata'],
-    additionalProperties: false
-  }
-
+  export const schema: JSONSchema<CampaignOutOfStockEvent> = createSchema(
+    Events.SubType.Rewards.CAMPAIGN_OUT_OF_STOCK,
+    campaignEventMetadataSchema
+  )
   export const validate: ValidateFunction<CampaignOutOfStockEvent> = generateLazyValidator(schema)
 }
 
 export namespace RewardDelayedEvent {
-  export const schema: JSONSchema<RewardDelayedEvent> = {
-    type: 'object',
-    properties: {
-      type: { type: 'string', const: Events.Type.REWARDS },
-      subType: { type: 'string', const: Events.SubType.Rewards.REWARD_DELAYED },
-      key: { type: 'string' },
-      timestamp: { type: 'number', minimum: 0 },
-      metadata: {
-        type: 'object',
-        properties: {
-          title: { type: 'string' },
-          description: { type: 'string' },
-          beneficiary: { type: 'string' },
-          tokenName: { type: 'string' },
-          tokenImage: { type: 'string' },
-          tokenRarity: { type: 'string', nullable: true },
-          tokenCategory: { type: 'string', nullable: true }
-        },
-        required: ['title', 'description', 'beneficiary', 'tokenName', 'tokenImage']
-      }
-    },
-    required: ['type', 'subType', 'key', 'timestamp', 'metadata'],
-    additionalProperties: false
-  }
-
+  export const schema: JSONSchema<RewardDelayedEvent> = createSchema(
+    Events.SubType.Rewards.REWARD_DELAYED,
+    rewardEventMetadataSchema
+  )
   export const validate: ValidateFunction<RewardDelayedEvent> = generateLazyValidator(schema)
 }
