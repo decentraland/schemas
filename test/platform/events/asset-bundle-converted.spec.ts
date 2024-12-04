df97bd427d86d382080a52aa50e09ebc502fb252
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2024-12-04 19:35:55 -0300

    test: add unit tests

---
+++ test/platform/events/asset-bundle-converted.spec.ts
@@ -0,0 +1,21 @@
+import expect from 'expect'
+import { AssetBundleConvertedEvent, Events } from '../../../src'
+
+describe('AssetBundleConverted Events tests', () => {
+  it('AssetBundleConvertedEvent static tests must pass', () => {
+    const event: AssetBundleConvertedEvent = {
+      type: Events.Type.ASSET_BUNDLE,
+      subType: Events.SubType.AssetBundle.CONVERTED,
+      key: 'key',
+      timestamp: 1,
+      metadata: {
+        entityId: 'baf',
+        platform: 'mac'
+      }
+    }
+
+    expect(AssetBundleConvertedEvent.validate(event)).toEqual(true)
+    expect(AssetBundleConvertedEvent.validate(null)).toEqual(false)
+    expect(AssetBundleConvertedEvent.validate({})).toEqual(false)
+  })
+})
