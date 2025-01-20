a85b3b90bee25850e3248c2ecce239c26b5a7633
LautaroPetaccio <lausuper@gmail.com>
2025-01-20 12:07:46 -0300

    feat: Add Contentful dapps schemas

---
+++ src/dapps/contentful/content-types.ts
@@ -0,0 +1,42 @@
+import { AlignmentFieldType, LocalizedField, SysLink } from './client'
+
+type MarketingAdminFields = {
+  name: LocalizedField<string>
+  campaign: LocalizedField<SysLink<'Entry'>>
+  marketplaceHomepageBanner: LocalizedField<SysLink<'Entry'>>
+  marketplaceCollectiblesBanner: LocalizedField<SysLink<'Entry'>>
+  marketplaceCampaignCollectiblesBanner: LocalizedField<SysLink<'Entry'>>
+  builderCampaignBanner: LocalizedField<SysLink<'Entry'>>
+}
+
+type CampaignFields = {
+  name: LocalizedField<string>
+  mainTag?: LocalizedField<string>
+  marketplaceTabName?: LocalizedField<string>
+  additionalTags?: LocalizedField<string[]>
+}
+
+type IBannerFields = {
+  id: LocalizedField<string>
+  showBanner: LocalizedField<boolean>
+  desktopTitle: LocalizedField<string>
+  mobileTitle: LocalizedField<string>
+  mobileTitleAlignment: LocalizedField<AlignmentFieldType>
+  desktopTitleAlignment: LocalizedField<AlignmentFieldType>
+  // Uses any to prevent installing the contentful types, but it should be LocalizedField<Document>
+  desktopText: LocalizedField<any>
+  // Uses any to prevent installing the contentful types, but it should be LocalizedField<Document>
+  mobileText: LocalizedField<any>
+  desktopTextAlignment: LocalizedField<AlignmentFieldType>
+  mobileTextAlignment: LocalizedField<AlignmentFieldType>
+  showButton: LocalizedField<boolean>
+  buttonLink?: LocalizedField<string>
+  buttonsText?: LocalizedField<string>
+  desktopButtonAlignment: LocalizedField<AlignmentFieldType>
+  mobileButtonAlignment: LocalizedField<AlignmentFieldType>
+  fullSizeBackground: LocalizedField<SysLink<'Asset'>>
+  mobileBackground: LocalizedField<SysLink<'Asset'>>
+  logo?: LocalizedField<SysLink<'Asset'>>
+}
+
+export { MarketingAdminFields, CampaignFields, IBannerFields }
