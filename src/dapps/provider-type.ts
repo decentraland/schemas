ea1a75b73c7c8de7078ac29bdd3daa0b377e9859
Juanma Hidalgo <juanma06@gmail.com>
2026-01-12 13:29:27 -0300

    feat: add thirdweb provider type (#402)

--- src/dapps/provider-type.ts
+++ src/dapps/provider-type.ts
@@ -14,7 +14,8 @@ export enum ProviderType {
   WALLET_CONNECT_V2 = 'wallet_connect_v2',
   WALLET_LINK = 'wallet_link',
   METAMASK_MOBILE = 'metamask_mobile',
-  AUTH_SERVER = 'auth_server'
+  AUTH_SERVER = 'auth_server',
+  THIRDWEB = 'thirdweb'
 }
 
 /**
