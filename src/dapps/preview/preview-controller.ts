e194822345d2cd822c63a26c8b16b6b1be8206c9
Rocío Mena <rociocmena@gmail.com>
2026-03-26 10:19:07 -0300

    Update setSpringBonesParams arg type

--- src/dapps/preview/preview-controller.ts
+++ src/dapps/preview/preview-controller.ts
@@ -47,5 +47,5 @@ export interface IEmoteController {
 }
 
 export interface IPhysicsController {
-  setSpringBonesParams(itemHash: string, params: SpringBoneParams[]): Promise<void>
+  setSpringBonesParams(itemHash: string, params: Record<string, SpringBoneParams>): Promise<void>
 }
